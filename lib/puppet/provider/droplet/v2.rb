8c309dafd5f59356348630f7630b9180f218f862
Gareth Rushgrove <gareth@morethanseven.net>
2014-08-11 13:47:21 +0100

    more style fixes

--- lib/puppet/provider/droplet/v2.rb
+++ lib/puppet/provider/droplet/v2.rb
@@ -1,42 +1,47 @@
 require 'barge'
 
-class Puppet::Provider::Droplet < Puppet::Provider
-  def initialize(*args)
-    @client = Barge::Client.new(access_token: ENV['DIGITALOCEAN_ACCESS_TOKEN'])
-    super(*args)
-  end
+module Puppet
+  class Provider
+    class Droplet < Puppet::Provider
+      def initialize(*args)
+        @client = Barge::Client.new(access_token:
+                                    ENV['DIGITALOCEAN_ACCESS_TOKEN'])
+        super(*args)
+      end
 
-  def exists?
-    Puppet.debug("Checking if droplet #{resource[:name]} exists")
-    _id_from_name(resource[:name]) ? true : false
-  end
+      def exists?
+        Puppet.debug("Checking if droplet #{resource[:name]} exists")
+        _id_from_name(resource[:name]) ? true : false
+      end
 
-  def create
-    Puppet.info('Creating new droplet')
-    response = @client.droplet.create(
-      name: resource[:name],
-      region: resource[:region],
-      size: resource[:size],
-      image: resource[:image])
-    if response.success?
-      Puppet.info("Created new droplet called #{resource[:name]}")
-    else
-      Puppet.error('Failed to create droplet')
+      def create
+        response = @client.droplet.create(
+          name: resource[:name],
+          region: resource[:region],
+          size: resource[:size],
+          image: resource[:image])
+        if response.success?
+          Puppet.info("Created new droplet called #{resource[:name]}")
+        else
+          Puppet.error('Failed to create droplet')
+        end
+      end
+
+      def destroy
+        Puppet.info("Destroying droplet #{resource[:name]}")
+        droplet = _id_from_name resource[:name]
+        if droplet
+          response = @client.droplet.destroy(droplet.id)
+          Puppet.error('Failed to destroy droplet') unless response.success?
+        end
+      end
+
+      private
+
+      def _id_from_name(name)
+        @client.droplet.all.droplets.find { |droplet| droplet.name == name }
+      end
     end
-  end
-
-  def destroy
-    Puppet.info("Destroying droplet #{resource[:name]}")
-    droplet = _id_from_name resource[:name]
-    if droplet
-      response = @client.droplet.destroy(droplet.id)
-      Puppet.error('Failed to destroy droplet') unless response.success?
-    end
-  end
-
-  private
-  def _id_from_name(name)
-    @client.droplet.all.droplets.find { |droplet| droplet.name == name }
   end
 end
 
