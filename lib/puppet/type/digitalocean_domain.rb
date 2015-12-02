d3fe43cc0955573dbca5b2e64d98fb6c0bde3fbd
Gareth Rushgrove <gareth@morethanseven.net>
2015-12-02 13:51:32 +0000

    reuse shared properties for domain

--- lib/puppet/type/digitalocean_domain.rb
+++ lib/puppet/type/digitalocean_domain.rb
@@ -1,27 +1,23 @@
+require 'puppet_x/garethr/digitalocean/property/boolean'
+
 Puppet::Type.newtype(:digitalocean_domain) do
-  @doc = 'type representing a digitalocean domain'
+  @doc = 'Type representing a digitalocean domain'
 
   ensurable
 
   newparam(:name, namevar: true) do
-    desc 'the name of the domain'
+    desc 'The name of the domain'
     validate do |value|
       fail 'Should not contains spaces' if value =~ /\s/
       fail 'Empty values are not allowed' if value == ''
     end
   end
 
-  newproperty(:zone_file) do
-    desc 'the full DNS zone'
-    validate do |value|
-      fail 'Zone is read-only'
-    end
+  newproperty(:zone_file, :parent => PuppetX::Garethr::DigitalOcean::Property::ReadOnly) do
+    desc 'The full DNS zone'
   end
 
-  newproperty(:ttl) do
-    desc 'the time to live for records on the domain'
-    validate do |value|
-      fail 'TTL is read-only'
-    end
+  newproperty(:ttl, :parent => PuppetX::Garethr::DigitalOcean::Property::ReadOnly) do
+    desc 'The time to live for records on the domain'
   end
 end
